3fcc4db99b9f6e82437cd591bb0d826c9b4e533d
IJMadalenA <developmentim@qaleon.com>
2023-04-26 15:32:41 +0200

    Upgrade.

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 # Script de instalación de Docker en Ubuntu/Debian
 
 # Actualizar índice de paquetes
-sudo apt-get update
+apt-get update
 
 # Instalar paquetes necesarios para descargar paquetes a través de HTTPS
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
 # Agregar la clave GPG oficial de Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -14,13 +14,13 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Actualizar el índice de paquetes nuevamente después de agregar el repositorio
-sudo apt-get update
+apt-get update
 
 # Instalar Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Agregar el usuario actual al grupo de docker para evitar tener que usar sudo cada vez que se utiliza Docker
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 
 # Comprobar la versión de Docker instalada
 docker --version
