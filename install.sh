696184698c3f1c1066deb316fc5855194ed7a1ea
IJMadalenA <developmentim@qaleon.com>
2023-04-26 13:52:02 +0200

    fixing install-docker.

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ apt-get install apt-transport-https ca-certificates curl software-properties-com
 
 # Descargar e instalar Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sh get-docker.sh
 
 apt-get update
 
 # Añadir el usuario actual al grupo de docker para evitar usar sudo
-sudo usermod -aG docker $(whoami)
-sudo usermod -aG docker jenkins
+usermod -aG docker $(whoami)
+usermod -aG docker jenkins
 
 # Reiniciar servicio Docker
-sudo systemctl jenkins restart
+systemctl jenkins restart
